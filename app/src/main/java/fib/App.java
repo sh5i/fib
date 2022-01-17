cc3149b7d35c8c829f3fde55ee31f41018f41d77
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2022-01-17 14:19:44 +0900

    Improve fib

--- app/src/main/java/fib/App.java
+++ app/src/main/java/fib/App.java
@@ -1,14 +1,16 @@
 package fib;
 
 public class App {
-    private static int fib(int n) {
+    private static int fibi(int n, int a, int b) {
         if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
+            return b;
         } else {
-            return fib(n - 2) + fib(n - 1);
+            return fibi(n - 1, b, a + b);
         }
+    }
+
+    private static int fib(int n) {
+        return fibi(n, 1, 0);
     }
 
     public static void main(String[] args) {
